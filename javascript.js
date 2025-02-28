e92c293efb55a34c971fe9ff60b4ab325ef1bfaf
yuta freitas <devyutalima@gmail.com>
2025-02-28 15:30:59 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let food1s = document.getElementById('food1');
+let food2s = document.getElementById('food2');
+let food = document.getElementById('food');
+
+food1s.addEventListener('click', ()=>{
+    food1s.style.backgroundImage="url('food1.png')";
+})
+
+food2s.addEventListener('click', ()=>{
+    food1s.style.backgroundImage="url('food.png')";
+})
